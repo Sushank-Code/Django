845bfa1b44ef1df986d93932f3fc7d0d80ef72b6
Sushank-Code <sushanklamsal7979@gmail.com>
2025-02-06 23:49:55 +0545

    Basics

--- tut3_project/app/views.py
+++ tut3_project/app/views.py
@@ -11,8 +11,18 @@ def index(request):
               'click3':'log in hello',
               'click4':'jijjoasdaskjdhaskhdjaskdh',
               'Date':datetime.now(),
-              'floatnum':51.89888898213
+              'floatnum':51.89888898213,
+              'alert':True,
+              'danger':"Notification",
             }
-    # return render(request,'app/index.html',context=course)
-    # return render(request,'app/index.html',context={'Name': 'Django2.0'})
-    return render(request,'app/index.html',course)
+    card ={
+        'cardname1' : 'Ecommerse',
+        'cardname2' : 'Blog',
+        'cardname3' : 'Video',
+    }
+    context = {'course': course, 'card': card}       # 2 dictionary or more
+    # return render(request,'app/index.html',context=course) # 1st approach
+    # return render(request,'app/index.html',course)         # 2nd approach
+    # return render(request,'app/index.html',context={'Name': 'Django2.0'}) # 3rd approach
+    return render(request,'app/index.html',context)
+    
--- tut3_project/tut3_project/settings.py
+++ tut3_project/tut3_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tut3_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates",],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,11 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 #Added manually
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
