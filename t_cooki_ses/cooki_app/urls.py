3438f022c77a40d44977202ff86432f42f5e77d1
Sushank-Code <sushanklamsal7979@gmail.com>
2025-04-07 00:42:42 +0545

    Django

--- t_cooki_ses/cooki_app/urls.py
+++ t_cooki_ses/cooki_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('setcookie/',views.setcookie,name='setcookie'),
     path('getcookie/',views.getcookie,name='getcookie'),
     path('delcookie/',views.delcookie,name='delcookie'),
+    path('setsession/',views.setsession,name='setsession'),
+    path('getsession/',views.getsession,name='getsession'),
+    path('delsession/',views.delsession,name='delsession'),
+    path('flush/',views.flushsession,name='flushsession'),
 ]
--- t_cooki_ses/cooki_app/views.py
+++ t_cooki_ses/cooki_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# cookie
+
 def setcookie(request):
     response = render(request,'cooki_app/set_cookie.html')
     response.set_cookie('auth_id','iiiiii')
@@ -23,3 +26,28 @@ def delcookie(request):
     response = render(request,'cooki_app/del_cookie.html',{'deleted_cookie':cookie_name})
     response.delete_cookie(cookie_name)
     return response
+
+# session
+
+def setsession(request):
+    request.session['fname'] = 'Sushank'
+    request.session['lname'] = 'Lamsal'
+    request.session['roll']= 101
+    return render(request,'cooki_app/set_session.html')
+
+def getsession(request):
+    all_session = request.session
+    print(all_session)
+    fid = request.session.get('fname')
+    return render(request,'cooki_app/get_session.html',{'all_session':all_session,
+                                                        'fid':fid})
+
+def delsession(request):
+    ses_name = 'lname'
+    if ses_name in request.session:
+        del request.session[ses_name]
+    return render(request,'cooki_app/del_session.html',{'del_session':ses_name})
+
+def flushsession(request):
+    request.session.flush()
+    return render(request,'cooki_app/flush_session.html')
